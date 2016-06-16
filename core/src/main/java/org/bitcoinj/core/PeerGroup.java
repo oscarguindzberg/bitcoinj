093d5e43e7e3465893e1a7dfd12c69172de33f0f
Manfred Karrer <mk@nucleo.io>
2016-06-16 16:40:07 +0200

    Move connection check to calling method

--- core/src/main/java/org/bitcoinj/core/PeerGroup.java
+++ core/src/main/java/org/bitcoinj/core/PeerGroup.java
@@ -535,7 +535,13 @@ public class PeerGroup implements TransactionBroadcaster {
                     executor.schedule(this, delay, TimeUnit.MILLISECONDS);
                     return;
                 }
-                connectTo(addrToTry, false, vConnectTimeoutMillis);
+
+                // BitcoinJ gets too many connections after a connection loss and reconnect as it adds up a lot of 
+                // potential candidates and then try to connect to all of those when getting connection again.
+                // A check for maxConnections is required to not exceed connections.
+                // TODO: Review
+                if(pendingPeers.size() + peers.size() >= maxConnections)
+                    connectTo(addrToTry, false, vConnectTimeoutMillis);
             } finally {
                 lock.unlock();
             }
@@ -1221,14 +1227,6 @@ public class PeerGroup implements TransactionBroadcaster {
         ver.time = Utils.currentTimeSeconds();
 
         Peer peer = new Peer(params, ver, address, chain, downloadTxDependencies);
-
-        // BitcoinJ gets too many connections after a connection loss and reconnect as it adds up a lot of 
-        // potential candidates and then try to connect to all of those when getting connection again.
-        // A check for maxConnections is required to not exceed connections.
-        // TODO: Review if the return of the peer cause any issues.
-        if(pendingPeers.size() + peers.size() >= maxConnections)
-            return peer;
-        
         peer.addEventListener(startupListener, Threading.SAME_THREAD);
         peer.setMinProtocolVersion(vMinRequiredProtocolVersion);
         pendingPeers.add(peer);

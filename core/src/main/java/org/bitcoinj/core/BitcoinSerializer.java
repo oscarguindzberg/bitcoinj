cfc93fc58e888e5d906564266381bf5ec89bf489
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-12-17 17:03:14 -0300

    Dash support: Ignore non-bitcoin messages when allowMoreInventoryTypes() and allowMoreMessages() is on.

--- core/src/main/java/org/bitcoinj/core/BitcoinSerializer.java
+++ core/src/main/java/org/bitcoinj/core/BitcoinSerializer.java
@@ -229,6 +229,8 @@ public class BitcoinSerializer extends MessageSerializer {
             return new UTXOsMessage(params, payloadBytes);
         } else if (command.equals("getutxos")) {
             return new GetUTXOsMessage(params, payloadBytes);
+        } else if(params.allowMoreMessages()) {
+            return new IgnoreThisMessage(params);
         } else {
             log.warn("No support for deserializing message with name {}", command);
             return new UnknownMessage(params, command, payloadBytes);

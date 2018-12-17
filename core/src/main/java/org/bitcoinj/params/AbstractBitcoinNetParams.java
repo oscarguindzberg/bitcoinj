53081dcbf0d49e27975ddaf1c992f36bfbe14a4f
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-12-17 17:25:08 -0300

    AbstractBitcoinNetParams: Optimize imports

--- core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
+++ core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
@@ -17,25 +17,16 @@
 
 package org.bitcoinj.params;
 
-import java.math.BigInteger;
-import java.util.concurrent.TimeUnit;
-
-import org.bitcoinj.core.Block;
-import org.bitcoinj.core.Coin;
-import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.StoredBlock;
-import org.bitcoinj.core.Transaction;
-import org.bitcoinj.core.Utils;
-import org.bitcoinj.utils.MonetaryFormat;
-import org.bitcoinj.core.VerificationException;
+import com.google.common.base.Stopwatch;
+import org.bitcoinj.core.*;
 import org.bitcoinj.store.BlockStore;
 import org.bitcoinj.store.BlockStoreException;
+import org.bitcoinj.utils.MonetaryFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Stopwatch;
-
-import org.bitcoinj.core.BitcoinSerializer;
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Parameters for Bitcoin-like networks.
